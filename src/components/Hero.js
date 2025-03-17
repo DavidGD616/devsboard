e7b44d34757d81d8abaccc55f4342ebc593098b9
David Guerrero Diaz <davidgd616@Davids-MacBook-Air.local>
2025-03-16 20:48:39 -0700

    Hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+export default function Hero() {
+  return (
+    <div className="flex flex-col items-center justify-center text-center my-12">
+      <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-6">
+        All Developer Jobs in One Place.
+      </h1>
+      <h2 className="text-xl md:text-2xl lg:text-3xl italic text-gray-600">
+        Brings together the latest dev job listings from multiple sources
+      </h2>
+    </div>
+  );
+}
