b7cf22f3aa4aa57e9c229da15b64f4f3a9f6a1d7
David Guerrero Diaz <davidgd616@Davids-MacBook-Air.local>
2025-03-23 14:38:15 -0700

    fething the titles to make the jobCards

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,21 @@
 import Hero from "@/components/Hero";
 import Search from "@/components/Search";
 
-export default function Home() {
+export default async function Home() { //Remember to delete the async once you export to the component 
+  const data = await fetch('http://localhost:3000/api/jobs');
+  const jobs = await data.json();
+  console.log("Jobs:", jobs);
+
+  const jobList = jobs[0].jobs;
   return (
     <div>
       <Hero />
       <Search />
+      <ul>
+        {jobList.map((job, index) => (
+          <li key={index}>{job.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
