44523cf7dfdc944890eb3698634a69dd892e1289
David Guerrero <davidguerrerodiaz12@gmail.com>
2025-03-24 18:50:35 -0700

    delete the fetch in the main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,8 @@
 import Hero from "@/components/Hero";
-import JobCard from "@/components/JobCard";
 import JobList from "@/components/JobList";
 import Search from "@/components/Search";
 
-export default async function Home() { //Remember to delete the async once you export to the component 
-  const data = await fetch('http://localhost:3000/api/jobs');
-  const jobs = await data.json();
-  console.log("Jobs:", jobs);
-
-  const jobList = jobs[0].jobs;
+export default async function Home() {
   return (
     <div>
       <Hero />
