c3e5df74cbf48af09ee023e120679ca180e6cb80
David Guerrero <davidguerrerodiaz12@gmail.com>
2025-03-24 17:44:43 -0700

    big layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="antialiased">
-        <div className="container mx-auto px-4">
+        <div className="container mx-auto px-4 lg:px-20">
           <Header />
           {children}
         </div>
