29b4bb1ad360547bb7581b8bdd84922b93c594ee
David Guerrero <davidguerrerodiaz12@gmail.com>
2025-03-26 19:34:20 -0700

    Testing how suspense work

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 import Hero from "@/components/Hero";
 import JobList from "@/components/JobList";
 import Search from "@/components/Search";
+import { Suspense } from "react";
 
 export default async function Home() {
   return (
     <div>
       <Hero />
       <Search />
-      <JobList />
+      < Suspense fallback={<p>Loading Jobs...</p>} >
+        <JobList />
+      </Suspense>
     </div>
   );
 }
