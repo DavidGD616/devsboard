e630959b20d97be8d45229bb206256f1cb8aa57d
David Guerrero <davidguerrerodiaz12@gmail.com>
2025-04-04 21:05:11 -0700

    supabase testing route working

---
+++ src/app/api/test-supabase/route.js
@@ -0,0 +1,25 @@
+import supabase from "@/utils/supabase/client";
+
+export async function GET(request) {
+    try {
+        const { data, error } = await supabase
+            .from('jobs')
+            .select('*')
+            .limit(1);
+
+        if (error) {
+            throw error;
+        }
+
+        return new Response(
+            JSON.stringify({ message: 'Connection succesful', data }),
+            { status: 200 }
+        )
+
+    } catch (error) {
+        return new Response(
+            JSON.stringify({ error: error.message }),
+            { status: 500 }
+        )
+    }
+}
